60b938de7819fcce78bb1e5c11a0145997eca89b
southnort <south-nort@ya.ru>
2024-04-07 20:34:33 +0300

    ReadOnlyAttribute, ShowIfAttribute

--- Runtime/Yrr/Utils/CsvReader.cs
+++ Runtime/Yrr/Utils/CsvReader.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
-using System;
 using System.IO;
+using System.Linq;
+using UnityEngine;
+
 
 namespace Yrr.Utils
 {
